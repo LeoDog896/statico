4a058c761b2f16fddde4d865ff10c4fb814fd2a6
LeoDog896 <LeoDog896@hotmail.com>
2022-10-26 15:37:06 +0000

    monaco!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,58 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import type monaco from 'monaco-editor';
+  import { onMount } from 'svelte';
+  import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+  import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+  import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+  import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+  import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+
+  let divEl: HTMLDivElement = null;
+  let editor: monaco.editor.IStandaloneCodeEditor;
+  let Monaco;
+
+  onMount(async () => {
+      // @ts-ignore
+      self.MonacoEnvironment = {
+          getWorker: function (_moduleId: any, label: string) {
+              if (label === 'json') {
+                  return new jsonWorker();
+              }
+              if (label === 'css' || label === 'scss' || label === 'less') {
+                  return new cssWorker();
+              }
+              if (label === 'html' || label === 'handlebars' || label === 'razor') {
+                  return new htmlWorker();
+              }
+              if (label === 'typescript' || label === 'javascript') {
+                  return new tsWorker();
+              }
+              return new editorWorker();
+          }
+      };
+
+      Monaco = await import('monaco-editor');
+      editor = Monaco.editor.create(divEl, {
+          value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+          language: 'javascript'
+      });
+
+      return () => {
+          editor.dispose();
+      };
+  });
+</script>
+
+<div bind:this={divEl} />
+
+<style>
+
+  :global(body) {
+    margin: 0;
+    padding: 0;
+  }
+
+  div {
+    height: 100vh;
+  }
+</style>
